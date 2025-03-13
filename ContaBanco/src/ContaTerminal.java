ec147d9b5ddde6ec7731f5232fb100c8dd4debc0
Ruan Victor <ruan.victor@aluno.uepb.edu.br>
2025-03-13 15:21:27 -0300

    alteraçoes

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Ola, Git");
+
+        Scanner sc=new Scanner(System.in);
+
+
+
+        System.out.println("Numero da agencia: ");
+        int numeroG=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Numero da conta: ");
+        String numeroC = sc.next();
+
+        System.out.println("NOME DO CLIENTE:  ");
+        String nome = sc.next();
+
+        System.out.println("Saldo: ");
+        double saldo = sc.nextDouble();
+        
+        System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agência é " + numeroG + " conta: " + numeroC + " e seu saldo é: R$" + String.format("%.2f", saldo) );
+        
     }
 }
